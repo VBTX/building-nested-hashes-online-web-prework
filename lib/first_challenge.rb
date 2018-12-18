ceb0b9722af962d469f978f2321fc60a4d3c8fcd
Victoria B <viktorija.bourassa@gmail.com>
2018-12-18 01:01:56 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,14 +1,15 @@
 def first_challenge
-   epic_tragedy = {:montague => {:patriarch => {},
+   epic_tragedy = {
+                    :montague => {:patriarch => {},
                                   :matriarch => {},
                                   :hero => {},
                                   :hero_friends => []
-                                },
+                                 },
                     :capulet => {
-                    :patriarch => {},
-                    :matriarch => {},
-                    :heroine => {},
-                    :heroine_friends => []
+                                 :patriarch => {},
+                                 :matriarch => {},
+                                 :heroine => {},
+                                 :heroine_friends => []
                      }
                    }
 end
